c87b80a3fe6a16f0fabe999b6518a79d53192337
DhawalV1 <97390574+DhawalV1@users.noreply.github.com>
2022-06-03 16:49:01 +0530

    Time: 145 ms (69.35%), Space: 14.8 MB (53.08%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -3,19 +3,50 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
+       
+        m = len(matrix)
+        n = len(matrix[0])
+        i = 0
+        while i < n:
+            if matrix[0][i] == 0:
+                count = 5
+                break
+            else:
+                count = 4
+            i = i + 1
+
+        j = 0
+        while j < m:
+            if matrix[j][0] == 0:
+                count1 = 6
+                break
+            else:
+                count1 = 3
+            j = j+1
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
                 if matrix[i][j] == 0:
-                    matrix[i][j] = 1.5
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if matrix[i][j] == 1.5:
+                        matrix[0][j] = matrix[i][0] = 0
+
+
+        for j in range(1,n):
+            if matrix[0][j] == 0:
+                for i in range(1,m):
                     matrix[i][j] = 0
-                    for k in range(len(matrix)):
-                        if matrix[k][j]!=1.5:
-                            matrix[k][j] = 0
-                    for l in range(len(matrix[0])):
-                        if matrix[i][l]!=1.5:
-                            matrix[i][l] = 0
-                
+
+        for i in range(1,m):
+            if matrix[i][0] == 0:
+                for j in range(1,n):
+                        matrix[i][j] = 0
+
+
+        if count==5:
+            for i in range(n):
+                matrix[0][i] = 0
+
+        if count1 == 6:
+            for i in range(m):
+                matrix[i][0] = 0
+                    
+        
         
