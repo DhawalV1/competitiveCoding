86bfb329968154389a673b315347430a8aabdfe4
DhawalV1 <97390574+DhawalV1@users.noreply.github.com>
2022-06-14 03:42:11 +0530

    Time: 122 ms (40.64%), Space: 14.3 MB (65.76%) - LeetHub

---
+++ 1115-print-foobar-alternately/1115-print-foobar-alternately.py
@@ -0,0 +1,23 @@
+class FooBar:
+    def __init__(self, n):
+        self.n = n
+        
+        self.lockFoo = threading.Lock()
+        self.lockFoo.acquire()
+        self.lockBar = threading.Lock()
+
+    def foo(self, printFoo):
+        for i in range(self.n):
+            self.lockBar.acquire() # make sure 'Bar' has been printed out before, and prevend any more print before 
+            printFoo()
+            self.lockFoo.release() # release so we can print 'Foo' next
+
+
+    def bar(self, printBar):
+        for i in range(self.n):
+            self.lockFoo.acquire() # make sure 'Foo' has been printed out before, and prevend any more print before 
+            printBar()
+            self.lockBar.release() # release so we can print 'Bar' next
+
+
+  
