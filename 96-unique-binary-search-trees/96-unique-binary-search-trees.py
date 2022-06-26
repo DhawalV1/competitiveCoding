2a17161f45726e50f9996f29dfc5189c9d18f169
DhawalV1 <97390574+DhawalV1@users.noreply.github.com>
2022-06-27 00:55:45 +0530

    Time: 52 ms (31.05%), Space: 13.9 MB (12.82%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.py
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.py
@@ -1,6 +1,19 @@
 class Solution:
     def numTrees(self, n: int) -> int:
-        return factorial(2*n)//factorial(n)//factorial(n)//(n+1)
+        dp ={}
+
+        def ct_tree(l):
+            if l<1:return 1
+
+            if l in dp:return dp[l]
+
+            dp[l]=0
+            for i in range(1,l+1):
+                dp[l] += ct_tree(i-1)*ct_tree(l-i)
+
+            return dp[l]
+
+        return ct_tree(n)
         
         
     
