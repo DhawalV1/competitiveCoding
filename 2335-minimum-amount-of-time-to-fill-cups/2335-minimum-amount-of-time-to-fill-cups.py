77c1c2d22603d94b8c9254fa00296dca62eda6c5
DhawalV1 <97390574+DhawalV1@users.noreply.github.com>
2022-07-11 23:20:05 +0530

    Time: 62 ms (8.33%), Space: 13.8 MB (100.00%) - LeetHub

--- 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.py
+++ 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.py
@@ -1,18 +1,6 @@
 class Solution:
     def fillCups(self, amount: List[int]) -> int:
         amount.sort()
-        res = 0
-        while amount[2] > 0:
-            
-            
-            amount[2] -= 1
-            res += 1
-            if amount[1] > 0:
-                amount[1] -= 1
-            amount.sort()
-            
-                
-        return res
-                
+        return max(amount[2], (sum(amount) + 1) // 2)
             
         
