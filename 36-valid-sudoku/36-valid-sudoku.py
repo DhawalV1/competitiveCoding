604071280ec0348680c323f7711e4526dc67c5d6
DhawalV1 <97390574+DhawalV1@users.noreply.github.com>
2022-07-12 16:13:18 +0530

    Time: 126 ms (38.41%), Space: 13.4 MB (68.75%) - LeetHub

---
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isValidSudoku(self, board):
+       
+        seen = set()
+        for i in xrange(0,9):
+            for j in xrange(0,9):
+                if board[i][j]!='.':
+                    
+                    cur = board[i][j]
+                    
+                    if (i,cur) in seen or (cur,j) in seen or (i/3,j/3,cur) in seen:
+                        return False
+                    seen.add((i,cur))
+                    seen.add((cur,j))
+                    seen.add((i/3,j/3,cur))
+                    
+        return True
+    
+        
