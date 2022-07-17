181430145e91f60d84909d09c7a797f877762602
DhawalV1 <97390574+DhawalV1@users.noreply.github.com>
2022-07-17 17:23:33 +0530

    Time: 1147 ms (37.02%), Space: 29.4 MB (23.28%) - LeetHub

--- 2195-append-k-integers-with-minimal-sum/2195-append-k-integers-with-minimal-sum.py
+++ 2195-append-k-integers-with-minimal-sum/2195-append-k-integers-with-minimal-sum.py
@@ -1,16 +1,13 @@
 class Solution:
     def minimalKSum(self, nums: List[int], k: int) -> int:
-        res = 0
-        cur = 1
-        nums.append(float('inf'))
-        nums.sort()
-        for i in range(len(nums)):
-            take = max(0,min(k,nums[i]-cur))
-            res += (cur+take)*(cur + take - 1)/2 - cur*(cur-1)/2
-            k -= take
-            cur = nums[i]+1
-        return int(res)
-        
+        sum = (k+1)*k//2
+        level = k+1
+        s = sorted(set(nums))
+        for x in s:
+            if x<level:
+                sum += level - x
+                level += 1
+        return sum
         
         
         
