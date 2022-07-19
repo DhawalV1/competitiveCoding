2d5177a5fda05b9a224f42cc37f96eefea316467
DhawalV1 <97390574+DhawalV1@users.noreply.github.com>
2022-07-19 21:18:54 +0530

    Time: 41 ms (64.46%), Space: 14 MB (11.70%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -10,13 +10,14 @@ class Solution:
             return b
         if n == 2:
             return c
-        j = 0
-        while j < n-2:
-            d = a + b + c
-            a = b
-            b = c
-            c = d
-            j = j + 1
-        return d
+        dp = [-1]*(n+1)
+        dp[0] = 0
+        dp[1] = 1
+        dp[2] = 1
+        
+        for i in range(3,n+1):
+            dp[i] = dp[i-3]+dp[i-2]+dp[i-1]
+        
+        return dp[-1]
         
         
