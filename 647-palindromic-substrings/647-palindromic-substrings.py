d3fdb236e1e3036f90377d5508126e171b1eef99
DhawalV1 <97390574+DhawalV1@users.noreply.github.com>
2022-07-22 05:05:40 +0530

    Time: 152 ms (77.65%), Space: 14 MB (32.07%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.py
+++ 647-palindromic-substrings/647-palindromic-substrings.py
@@ -1,5 +1,32 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
+        res = 0
+        n = len(s)
+        def palini(i,j):
+            res = 0
+            while i>=0 and j<n:
+                if s[i]!=s[j]:
+                    return res
+                i -= 1
+                j +=1
+                res += 1
+            
+            return res
+        
+        for i in range(n):
+            res += palini(i,i) + palini(i-1,i)
+            
+        return res
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
         count = 0
         for i in range(len(s)):
             for j in range(i,len(s)):
