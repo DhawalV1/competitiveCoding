91a79ac3bea3026fbf8f47fa139de1306f3b6608
DhawalV1 <97390574+DhawalV1@users.noreply.github.com>
2022-07-21 07:10:05 +0530

    Time: 358 ms (59.46%), Space: 14.3 MB (41.96%) - LeetHub

---
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
@@ -0,0 +1,28 @@
+import heapq
+class Solution:
+    def getKth(self, lo: int, hi: int, k: int) -> int:
+        self.dic = {1:0}
+        for i in range(lo,hi+1):
+            self.power(i)
+            
+        lst = [(self.dic[i],i) for i in range(lo,hi+1)]
+        heapq.heapify(lst)
+        
+        for j in range(k):
+            ans = heapq.heappop(lst)
+            
+        return ans[1]
+        
+    def power(self,n):
+        if n in self.dic:
+            return self.dic[n]
+        if n%2:
+            self.dic[n] = self.power(3*n+1)+1
+        else:
+            self.dic[n] = self.power(n>>1)+1
+        return self.dic[n]
+            
+        
+        
+        
+        
