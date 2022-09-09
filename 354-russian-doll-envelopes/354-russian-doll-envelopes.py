1a9bbf0db0ae8c58cf7740ea3f4d7f2c1fe35eb5
DhawalV1 <97390574+DhawalV1@users.noreply.github.com>
2022-09-10 01:35:45 +0530

    Time: 2871 ms (9.68%), Space: 61.8 MB (43.67%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.py
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.py
@@ -1,16 +1,17 @@
 class Solution:
     def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
         envelopes.sort(key=lambda x: (x[0],-x[1]))
-        searchi = []
-        for i in range(len(envelopes)):
-            searchi.append(envelopes[i][1])
+        
+        
         sub = []
-        for x in searchi:
-            if len(sub)==0 or sub[-1]<x:
-                sub.append(x)
+        sub.append(envelopes[0][1])
+        for i in range(1,len(envelopes)):
+            
+            if sub[-1]<envelopes[i][1]:
+                sub.append(envelopes[i][1])
             else:
-                idx = bisect_left(sub,x)
-                sub[idx] = x
+                idx = bisect_left(sub,envelopes[i][1])
+                sub[idx] = envelopes[i][1]
         return len(sub)
             
             
