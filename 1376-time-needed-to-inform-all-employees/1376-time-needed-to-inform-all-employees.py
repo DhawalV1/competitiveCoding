938b10abbb843df324f5b4fa0304ae846a7aaa50
DhawalV1 <97390574+DhawalV1@users.noreply.github.com>
2022-09-14 05:28:17 +0530

    Time: 1215 ms (99.22%), Space: 39.6 MB (83.20%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
@@ -10,3 +10,20 @@ class Solution:
             dfs(eid)
         return max(informTime)
     
+        ''''
+        if n <= 1:
+            return 0
+        rst = 0
+        childs = defaultdict(list)
+        for idx, parent in enumerate(manager):
+            childs[parent].append(idx)
+
+        q = deque([(headID, informTime[headID])])
+        while q:
+            cur_id, cur_time = q.popleft()
+            # calculate max
+            rst = max(rst, cur_time)
+            for child in childs[cur_id]:
+                q.append((child, cur_time + informTime[child]))
+        return rst 
+        '''
