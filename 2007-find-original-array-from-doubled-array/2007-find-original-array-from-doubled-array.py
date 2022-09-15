7f5cec74cffd3e8323e421c2a274b3125a30f3a6
DhawalV1 <97390574+DhawalV1@users.noreply.github.com>
2022-09-15 18:16:33 +0530

    Time: 2657 ms (31.34%), Space: 32.1 MB (75.26%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
@@ -2,7 +2,7 @@ class Solution:
     def findOriginalArray(self,arr: List[int]) -> List[int]:
         
         cnt, ans = Counter(arr), []
-        for num in sorted(arr, key = lambda x: abs(x)):
+        for num in sorted(arr):
             if cnt[num] == 0: continue
             if cnt[2*num] == 0: return []
             ans += [num]
