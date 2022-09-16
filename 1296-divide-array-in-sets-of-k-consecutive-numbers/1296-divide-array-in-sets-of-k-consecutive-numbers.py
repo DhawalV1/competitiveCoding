f7a1ee80d2d6df597267b7570a9e944c86cd0977
DhawalV1 <97390574+DhawalV1@users.noreply.github.com>
2022-09-16 09:47:47 +0530

    Time: 378 ms (100.00%), Space: 31.4 MB (59.48%) - LeetHub

--- 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.py
+++ 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.py
@@ -1,11 +1,30 @@
 class Solution:
-    def isPossibleDivide(self, A: List[int], k: int) -> bool:
-        c = collections.Counter(A)
-        start = collections.deque()
-        last_checked, opened = -1, 0
-        for i in sorted(c):
-            if opened > c[i] or opened > 0 and i > last_checked + 1: return False
-            start.append(c[i] - opened)
-            last_checked, opened = i, c[i]
-            if len(start) == k: opened -= start.popleft()
-        return opened == 0
+    def isPossibleDivide(self, hand: List[int], group: int) -> bool:
+        n = len(hand)
+        groups = n / group
+        if int(groups) == groups:
+            d = {}
+            for i in hand:
+                if i in d:
+                    d[i] += 1
+                else:
+                    d[i] = 1
+            while groups:
+                m = min(d)
+                if m in d:
+                    v = d[m]
+                    for i in range(m, m + group):
+                        if i in d:
+                            d[i] -= v
+                            if d[i] < 0:
+                                return False
+                            elif d[i] == 0:
+                                d.pop(i)                        
+                        else:
+                            return False
+                    else:
+                        groups -= v
+            else:
+                return True
+        else:
+            return False
