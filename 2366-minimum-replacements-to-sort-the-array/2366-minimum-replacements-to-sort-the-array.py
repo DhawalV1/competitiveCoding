c22f6185a0b3240afde4d460728885970c0ccb87
DhawalV1 <97390574+DhawalV1@users.noreply.github.com>
2022-09-16 08:16:40 +0530

    Time: 1529 ms (5.03%), Space: 28.9 MB (10.54%) - LeetHub

---
+++ 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.py
@@ -0,0 +1,16 @@
+from math import ceil
+class Solution:
+    def minimumReplacement(self, nums: List[int]) -> int:
+        n = len(nums)
+        ret = 0
+        prev = nums[n - 1]
+        for ind in range(n - 2, -1, -1):
+            num = nums[ind]
+            k = ceil(num / prev)
+
+            # (k - 1) is the minimum number of times you'll have to split
+            ret += k - 1
+            # (num // k) is the maximal number you can create from splitting (k - 1) times
+            prev = num // k
+
+        return ret
