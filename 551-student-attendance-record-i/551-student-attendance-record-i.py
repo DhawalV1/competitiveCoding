eab36f66e11dce34b0e90993b47bb1eadfe468da
DhawalV1 <97390574+DhawalV1@users.noreply.github.com>
2022-09-21 03:49:51 +0530

    Time: 58 ms (27.87%), Space: 13.8 MB (57.03%) - LeetHub

--- 551-student-attendance-record-i/551-student-attendance-record-i.py
+++ 551-student-attendance-record-i/551-student-attendance-record-i.py
@@ -1,17 +1,7 @@
 class Solution:
     def checkRecord(self, s: str) -> bool:
-        s = s + 'PP'
-        count = 0
-        for i in range(len(s)-2):
-            if s[i] == 'A':
-                count += 1
-                if count > 1:
-                    return False
-                    
-            elif s[i]=='L':
-                if s[i+1]=='L':
-                    if s[i+2]=='L':
-                        return False
-            else:
-                continue
+        absent=s.count("A")
+        if absent>=2 or "LLL" in s:
+            return False
         return True
+            
