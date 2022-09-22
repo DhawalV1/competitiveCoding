c5042fde0c53a3065e692deba66cbe67c14400b9
DhawalV1 <97390574+DhawalV1@users.noreply.github.com>
2022-09-22 11:42:48 +0530

    Time: 183 ms (55.38%), Space: 55.3 MB (6.28%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -4,7 +4,7 @@ class Solution:
         for a in A:
             if len(set(a)) < len(a): continue
             a = set(a)
-            for c in dp[:]:
+            for c in dp:
                 if a & c: continue
                 dp.append(a | c)
         return max(len(a) for a in dp)
