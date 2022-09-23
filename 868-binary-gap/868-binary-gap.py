634a0a85fd82fc0b9b747a93f3e7e90a8e7c0b37
DhawalV1 <97390574+DhawalV1@users.noreply.github.com>
2022-09-23 06:49:57 +0530

    Time: 34 ms (87.82%), Space: 13.9 MB (10.38%) - LeetHub

--- 868-binary-gap/868-binary-gap.py
+++ 868-binary-gap/868-binary-gap.py
@@ -1,7 +1,15 @@
 class Solution:
-    def binaryGap(self, N: int) -> int:
-        index = [i for i, v in enumerate(bin(N)) if v == '1']
-        return max([b - a for a, b in zip(index, index[1:])] or [0])
-                
-            
+    def binaryGap(self, n: int) -> int:
+        bits=bin(n)
+        bits=bits[2:]
+        ans=0
+        i=0
+        j=0
+        while j<len(bits):
+            if bits[j]=="1":
+                if (j-i)>ans:
+                    ans=j-i
+                i=j
+            j=j+1
+        return ans
         
