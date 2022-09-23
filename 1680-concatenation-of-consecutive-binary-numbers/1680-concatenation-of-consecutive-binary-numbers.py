345e5547ae8ea2268270822dd61fef20282571bb
DhawalV1 <97390574+DhawalV1@users.noreply.github.com>
2022-09-23 23:33:31 +0530

    Time: 6986 ms (5.51%), Space: 13.9 MB (80.31%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.py
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.py
@@ -1,6 +1,15 @@
 class Solution:
     def concatenatedBinary(self, n: int) -> int:
-        return self.helper(n)
+        #return self.helper(n)
+        mod = 10**9 + 7
+        ans = 0
+        
+        for i in range(1,n+1):
+            b = math.floor(math.log2(i))+1
+            ans = ((ans<<b)%mod + i)%mod
+            
+        return ans
+    ''''
     def helper(self,n):
         mod = 10**9 + 7
         if n == 1:
@@ -8,4 +17,4 @@ class Solution:
         p = bin(n)[2:]
         
         return (self.helper(n-1)<<len(p))%mod + n
-        
+    '''
