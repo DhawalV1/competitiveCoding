8ba977412831560213b916182c10790213f631c8
DhawalV1 <97390574+DhawalV1@users.noreply.github.com>
2022-09-26 01:26:31 +0530

    Time: 107 ms (54.04%), Space: 15.6 MB (97.49%) - LeetHub

--- 260-single-number-iii/260-single-number-iii.py
+++ 260-single-number-iii/260-single-number-iii.py
@@ -1,7 +1,14 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
         
-        s = reduce(xor, nums)
-        nz = s & (s-1) ^ s
-        num1 = reduce(xor, filter(lambda n: n & nz, nums))
-        return(num1, s ^ num1)
+        n1 = 0
+        n2 = 0
+        p=reduce(operator.xor,nums)
+        bit = p & ~(p-1)
+        for num in nums:
+            if num&bit>0:
+                n1^=num
+            else:
+                n2^=num
+                
+        return [n1,n2]
