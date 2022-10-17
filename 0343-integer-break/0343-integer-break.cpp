b2dea35a5d898e77d58deedda98d6a34666c2c23
DhawalV1 <97390574+DhawalV1@users.noreply.github.com>
2022-10-18 03:29:08 +0530

    Time: 3 ms (46.38%), Space: 6.2 MB (29.45%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int integerBreak(int n) {
+        if (n==2) return 1;
+        if (n==3) return 2;
+        if (n==4) return 4;
+        if (n%3==0) return pow(3,int(n/3));
+        else if (n%3==1) return 4*pow(3,int((n-4)/3));
+        else return 2*pow(3,int((n-2)/3));
+        
+       
+    }
+};
