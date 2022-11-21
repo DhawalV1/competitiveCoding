ef4acc2b18dff2e8f05daf71d61eed924ac9acd7
DhawalV1 <97390574+DhawalV1@users.noreply.github.com>
2022-11-22 03:28:26 +0530

    Time: 37 ms (82.09%), Space: 13.9 MB (69.65%) - LeetHub

--- 2310-sum-of-numbers-with-units-digit-k/2310-sum-of-numbers-with-units-digit-k.py
+++ 2310-sum-of-numbers-with-units-digit-k/2310-sum-of-numbers-with-units-digit-k.py
@@ -3,7 +3,7 @@ class Solution:
         
         if num == 0:return 0
         for i in range(1,11):
-            if i*k%10==num%10 and i*k<=num:
+            if (i*k)%10==num%10 and i*k<=num:
                 return i
             
         return -1
