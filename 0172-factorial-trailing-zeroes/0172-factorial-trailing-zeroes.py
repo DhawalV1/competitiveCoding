d66cf46737ac9aa3e778e29befc06b8514976ce7
DhawalV1 <97390574+DhawalV1@users.noreply.github.com>
2022-11-28 02:52:26 +0530

    Time: 57 ms (70.04%), Space: 13.9 MB (62.11%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
@@ -0,0 +1,11 @@
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        if n == 0: return 0
+        m1 = n//5
+        m2 = n//25
+        m3 = (n//125)
+        m4 = (n//625)
+        m5 = n//3125
+        
+        
+        return m1 + m2 + m3 + m4 + m5
