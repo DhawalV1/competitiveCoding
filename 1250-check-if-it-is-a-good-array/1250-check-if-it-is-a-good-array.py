2080075e697aeaa4936865929b56a8ee35b5fdba
DhawalV1 <97390574+DhawalV1@users.noreply.github.com>
2022-11-30 03:13:12 +0530

    Time: 765 ms (20.39%), Space: 24.9 MB (23.30%) - LeetHub

--- 1250-check-if-it-is-a-good-array/1250-check-if-it-is-a-good-array.py
+++ 1250-check-if-it-is-a-good-array/1250-check-if-it-is-a-good-array.py
@@ -1,4 +1,19 @@
 class Solution:
     def isGoodArray(self, nums: List[int]) -> bool:
-        return True if reduce(gcd, nums) == 1 else False
+        if nums == [1]:return True
+        if len(nums)==1:return False
+        k = self.gcde(nums[0],nums[1])
+        if k == 1:return True
+        for i in range(2,len(nums)):
+            k = self.gcde(k,nums[i])
+            if k == 1:return True
+        return False
+            
+    def gcde(self,m,n):
+        if m>=n:
+            if m%n==0:return n
+            return self.gcde(m%n,n)
+        elif m<n:
+            if n%m==0:return m
+            return self.gcde(n%m,m)
         
