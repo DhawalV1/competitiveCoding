d6acccfc06dab2332e7b0e7f494dc783be0573e2
DhawalV1 <97390574+DhawalV1@users.noreply.github.com>
2022-12-01 13:07:02 +0530

    Time: 73 ms (38.28%), Space: 13.8 MB (77.72%) - LeetHub

---
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.py
@@ -0,0 +1,15 @@
+class Solution:
+    def halvesAreAlike(self, s: str) -> bool:
+        count = count2 = 0
+        seen = set(('a','e','i','o','u','A','E','I','O','U'))
+        n = len(s)//2
+        for i in range(len(s)):
+            if i < n:
+                if s[i] in seen:
+                    count += 1
+            else:
+                if s[i] in seen:
+                    count2 += 1
+                    
+        return count == count2
+        
