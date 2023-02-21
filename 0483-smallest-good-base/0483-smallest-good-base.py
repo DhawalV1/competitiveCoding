e9af61dc4eb861fe4cc25cde3bc5f6940a698d2c
DhawalV1 <97390574+DhawalV1@users.noreply.github.com>
2023-02-21 19:43:36 +0530

    Time: 32 ms (86.02%), Space: 13.9 MB (53.76%) - LeetHub

---
+++ 0483-smallest-good-base/0483-smallest-good-base.py
@@ -0,0 +1,16 @@
+class Solution:
+    def smallestGoodBase(self, n: str) -> str:
+        
+        n = int(n)
+        
+        maxm = int(math.log(n,2))
+        
+        for m in range(maxm,1,-1):
+            
+            k = int(n**m**-1)
+            
+            if (k**(m+1)-1)//(k-1) == n:
+                
+                return str(k)
+            
+        return str(n-1)
