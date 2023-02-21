f082859990a472db631b173bb96d116d7b999d08
DhawalV1 <97390574+DhawalV1@users.noreply.github.com>
2023-02-21 13:18:18 +0530

    Time: 185 ms (45.30%), Space: 23.6 MB (99.62%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        
+        a = nums[0]
+        
+        for i in nums[1:]:
+            
+            a = a^i
+            
+        return a
