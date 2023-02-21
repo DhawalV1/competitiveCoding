2fcae367ea5f054350ec4323e98b6f808129932b
DhawalV1 <97390574+DhawalV1@users.noreply.github.com>
2023-02-21 13:52:54 +0530

    Time: 187 ms (39.95%), Space: 23.8 MB (39.30%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -1,10 +1,15 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
         
-        a = nums[0]
         
-        for i in nums[1:]:
-            
-            a = a^i
-            
-        return a
+        
+        L, R, mid, ans = 0, len(nums)-1, 0, 0
+        while L <= R:
+            mid = ((L + R) >> 2) << 1  # Does the same thing as above, i.e, ensuring mid is even
+            if mid+1 < len(nums) and nums[mid] == nums[mid+1]:
+                L = mid + 2
+            else: 
+                R, ans = mid - 1, nums[mid]
+        return ans
+                
+                
