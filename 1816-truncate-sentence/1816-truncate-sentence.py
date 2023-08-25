aee4cb98c7ee2728c846eba433f8c61f8a5a3fa7
DhawalV1 <97390574+DhawalV1@users.noreply.github.com>
2023-08-26 04:11:30 +0530

    Time: 48 ms (23.66%), Space: 16.4 MB (36.39%) - LeetHub

---
+++ 1816-truncate-sentence/1816-truncate-sentence.py
@@ -0,0 +1,10 @@
+class Solution:
+    def truncateSentence(self, s: str, k: int) -> str:
+        count=0
+        for i,c in enumerate(s):
+            if c==' ':
+                count+=1
+                if count==k:
+                    return s[:i]
+        return s
+        
