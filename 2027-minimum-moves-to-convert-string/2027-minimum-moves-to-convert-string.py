d3add8495a600c22ecbffbcacbdf88fc354a23aa
DhawalV1 <97390574+DhawalV1@users.noreply.github.com>
2023-09-16 11:20:50 +0530

    Time: 43 ms (41.52%), Space: 16.1 MB (92.40%) - LeetHub

--- 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.py
+++ 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.py
@@ -8,19 +8,12 @@ class Solution:
                 i += 1
                 continue
                 
-            elif s[i] == 'X':
-                if s[i+1:i+3]=='OO':
-                    count += 1
-                    i +=3
-                elif s[i+1:i+3]=='XO':
-                    count += 1
-                    i +=3
-                elif s[i+1:i+3]=='OX':
-                    count += 1
-                    i +=3
-                else:
-                    count += 1
-                    i +=3
+            else:
+                
+                
+                count += 1
+                i +=3
+                
                     
         return count
                 
