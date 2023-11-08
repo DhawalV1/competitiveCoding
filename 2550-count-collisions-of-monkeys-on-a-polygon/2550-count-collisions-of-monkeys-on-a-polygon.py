2b61651b0a86db31b35467e994fb5fee5506a58e
DhawalV1 <97390574+DhawalV1@users.noreply.github.com>
2023-11-08 14:36:54 +0530

    Time: 37 ms (62.71%), Space: 16.2 MB (63.11%) - LeetHub

--- 2550-count-collisions-of-monkeys-on-a-polygon/2550-count-collisions-of-monkeys-on-a-polygon.py
+++ 2550-count-collisions-of-monkeys-on-a-polygon/2550-count-collisions-of-monkeys-on-a-polygon.py
@@ -1,5 +1,5 @@
 class Solution:
     def monkeyMove(self, n: int) -> int:
-        if pow(2,n,1000000007)==1:return 1000000006
-        return pow(2,n,1000000007)-2
+        
+        return (pow(2,n,1000000007)-2)%1000000007
         
