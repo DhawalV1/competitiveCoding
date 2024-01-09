a9e388855b0b0059a2ab8b5b9a15e2348dda0311
DhawalV1 <97390574+DhawalV1@users.noreply.github.com>
2024-01-10 00:34:58 +0530

    Time: 35 ms (83.93%), Space: 17.4 MB (7.47%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.py
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.py
@@ -6,12 +6,19 @@
 #         self.right = right
 class Solution:
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        return self.findleaf(root1) == self.findleaf(root2)
         
+    def findleaf(self, root):
+        if not root: return []
+        if not (root.left or root.right): return [root.val]
+        return self.findleaf(root.left) + self.findleaf(root.right)
+    '''
         ret = []
         res = []
         self.dfs(root1,ret)
-        self.dfs(root2,res)
-        return ret == res
+        b = ret[::-1]
+        self.dfs1(root2,b)
+        return len(b) == 0
         
         
     def dfs(self,node,ret):
@@ -23,4 +30,26 @@ class Solution:
             
         self.dfs(node.left,ret)
         self.dfs(node.right,ret)
+        
+    def dfs1(self,node,ret):
+        
+        if node is None: return
+        
+        if node.left is None and node.right is None:
+            if ret[-1] != node.val:
+                return False
+            else:
+                ret.pop()
+            
+        self.dfs(node.left,ret)
+        self.dfs(node.right,ret)
+        
+        
+    '''
+    def dfs(self,root):
+        if root is None: return 
+        if not(root.left or root.right) is None:return [root.val]
+        return self.dfs(root.left)+self.dfs(root.right)
+        
+            
         
