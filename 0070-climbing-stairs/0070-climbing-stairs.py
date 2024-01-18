6219bbfe084d25918d498dce6072630ac9c68ddb
DhawalV1 <97390574+DhawalV1@users.noreply.github.com>
2024-01-18 10:30:22 +0530

    Time: 41 ms (33.21%), Space: 17.2 MB (45.94%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,6 +1,6 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        
+        '''
         cache = {}
         
         def helper(n):
@@ -21,5 +21,12 @@ class Solution:
             cache[n] = res
             return res
         return helper(n)
-            
-        
+        ''' 
+        if n<3:
+            return n
+        dp = [0]*n
+        dp[0] = 1
+        dp[1] = 2
+        for i in range(2,n):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n-1]
