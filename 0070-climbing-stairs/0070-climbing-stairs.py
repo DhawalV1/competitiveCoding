f1961efd0b5b924ce03f1c1553e32ddf839a4f36
DhawalV1 <97390574+DhawalV1@users.noreply.github.com>
2024-01-18 10:33:09 +0530

    Time: 34 ms (75.77%), Space: 17.4 MB (15.77%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -22,11 +22,5 @@ class Solution:
             return res
         return helper(n)
         ''' 
-        if n<3:
-            return n
-        dp = [0]*n
-        dp[0] = 1
-        dp[1] = 2
-        for i in range(2,n):
-            dp[i] = dp[i-1] + dp[i-2]
-        return dp[n-1]
+        
+        return int(round((((1 + 5**0.5)/2.0)**(n+1) - (1-((1 + 5**0.5)/2.0))**(n+1)) / 5**0.5))
