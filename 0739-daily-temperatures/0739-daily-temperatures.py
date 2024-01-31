9fe13e656c1adbad51de9d519b6dd8a1a9020004
DhawalV1 <97390574+DhawalV1@users.noreply.github.com>
2024-01-31 14:09:51 +0530

    Time: 881 ms (88.33%), Space: 30.5 MB (90.04%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -9,26 +9,5 @@ class Solution:
                 wait[j] = i-j
             stack.append(i)
         return wait
-        '''
-        t.append(0)
-        res = []
-        i = 0
-        j = i + 1
-        while i<j and j<len(t):
-            while t[i]>=t[j] and j!=len(t)-1:
-                j = j+1
-            else:
-                if t[i]<t[j]: 
-                    res.append(j-i)
-                    i = i+1
-                    j = i+1
-                else:
-                    res.append(0)
-                    i = i+1
-                    j = i+1
-                    
-            
         
-        return res
         
-        '''
