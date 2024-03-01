f9740fa5d310999f7161b10e236070a542df3bfd
DhawalV1 <97390574+DhawalV1@users.noreply.github.com>
2024-03-01 06:26:47 +0530

    Time: 47 ms (77.72%), Space: 16.6 MB (74.46%) - LeetHub

--- 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.py
+++ 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.py
@@ -1,7 +1,10 @@
 class Solution:
     def numOfWays(self, n: int) -> int:
         mod = 10**9+7
-        a = [[6,6]]
+        col1 = 6
+        col2 = 6
         for i in range(n-1):
-            a.append([(a[i][0]*2%mod+a[i][1]*2%mod)%mod,(a[i][0]*2%mod+a[i][1]*3%mod)%mod])
-        return sum(a[-1])%mod
+            temp = col1
+            col1 = (col1*2%mod+col2*2%mod)%mod
+            col2 = (temp*2%mod+col2*3%mod)%mod
+        return (col1+col2)%mod
